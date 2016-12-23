1d2f59d3eb18953fb4344be1a0f4deef80dfd59e
Abdullah Çetinkaya <mail@abdullahcetinkaya.com.tr>
2016-12-24 00:05:54 +0200

    Create
    Create

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "\e[40;38;5;82m Linux  \e[30;48;5;82m Software Bash v1 \e[0m"
+
+sudo apt-get update 
+sudo apt-get install filezilla
+sudo apt-get install mc
+sudo apt-get install youtube-dl
